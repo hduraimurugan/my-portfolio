bec08d45477d62ca40b924f8c5ff99486d8f04e6
Duraimurugan-H <hduraimurugan@gmail.com>
2024-09-05 20:36:58 +0530

    ctct btn added and some changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import HeroImg from '../assets/hero.png';
-import Hero2Img from '../assets/hero2.png';
+// import Hero2Img from '../assets/hero2.png';
 import Hero3Img from '../assets/hero3.png';
 import bgImg from '../assets/bg.gif';
 import { AiOutlineTwitter, AiOutlineLinkedin, AiOutlineGithub, AiOutlineMedium, AiOutlineInstagram } from "react-icons/ai";
+import { TbMessage } from "react-icons/tb";
+
 
 const Hero = () => {
     return (
@@ -23,7 +25,7 @@ const Hero = () => {
                             Hi,
                         </h1>
                         <h1 className='animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 md:text-6xl text-3xl text-white  font-list-font font-bold'>
-                        It's Duraimurugan H,</h1>
+                            It's Duraimurugan H,</h1>
 
                         <p className='md:text-4xl text-xl mt-3'>
                             I am a <span className='text-indigo-300 md:text-5xl text-2xl font-extrabold'>MERN</span> stack Developer
@@ -37,10 +39,20 @@ const Hero = () => {
                         {/* <a href='https://x.com/hduraimurugan16' target="_blank" className='hover:text-indigo-400'><AiOutlineTwitter size={34} /></a> */}
                         {/* <a href='https://www.instagram.com/duraimurugan_16/' target="_blank" className='hover:text-indigo-400'><AiOutlineInstagram size={34} /></a> */}
                     </div>
+
+                    <div className='drop-shadow-md mt-16'>
+                        <div className='flex'>
+                            <a className='btn-prj font-list-font text-xl p-3 px-6 rounded-full flex gap-1' href='#contact'>Contact Me <TbMessage size={25} /></a>
+                        </div>
+                    </div>
                 </div>
 
-                <div className='py-5 md:w-1/3'>
-                    <img className='mix-blend-lighten' src={HeroImg} alt='hero' />
+                <div className='py-5 px-30 md:w-1/4'>
+                    {/* <img className='mix-blend-lighten' src={HeroImg} alt='hero' /> */}
+                    <div className='magicpattern mix-blend-lighten md:relative'>
+                        <img className='img-fluid md:absolute md:bottom-12' src={Hero3Img} alt='hero'></img>
+                    </div>
+
                 </div>
             </section>
         </>
